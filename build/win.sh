9ccb75955a0e3d0cb8ba8f7e485a06362d2dad9b
Lovell Fuller <github@lovell.info>
2017-09-22 18:54:52 +0100

    Bootstrap with snapshot of sharp/packaging

---
+++ build/win.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+VERSION_VIPS_MAJOR=$(echo $VERSION_VIPS | cut -d. -f1)
+VERSION_VIPS_MINOR=$(echo $VERSION_VIPS | cut -d. -f2)
+
+# Fetch and unzip
+mkdir /vips
+cd /vips
+curl -L -O https://github.com/lovell/build-win64/releases/download/v${VERSION_VIPS}/vips-dev-w64-web-${VERSION_VIPS}.zip
+unzip vips-dev-w64-web-${VERSION_VIPS}.zip
+
+# Clean and zip
+cd /vips/vips-dev-${VERSION_VIPS_MAJOR}.${VERSION_VIPS_MINOR}
+rm bin/libvipsCC-42.dll bin/libvips-cpp-42.dll bin/libgsf-win32-1-114.dll bin/libgettext*.dll
+cp bin/*.dll lib/
+cp -r lib64/* lib/
+
+echo "Creating tarball"
+tar czf /packaging/libvips-${VERSION_VIPS}-${PLATFORM}.tar.gz include lib/glib-2.0 lib/libvips.lib lib/libglib-2.0.lib lib/libgobject-2.0.lib lib/*.dll
+echo "Shrinking tarball"
+advdef --recompress --shrink-insane /packaging/libvips-${VERSION_VIPS}-${PLATFORM}.tar.gz
