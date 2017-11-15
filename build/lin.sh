f39f7803fb7a3deb1ca3e6e0825ccb8c391790f8
Lovell Fuller <github@lovell.info>
2017-11-15 19:21:07 +0000

    Ensure latest versions via release-monitoring check

--- build/lin.sh
+++ build/lin.sh
@@ -18,30 +18,71 @@ export CXXFLAGS="${FLAGS}"
 # Dependency version numbers
 VERSION_ZLIB=1.2.11
 VERSION_FFI=3.2.1
-VERSION_GLIB=2.53.5
-VERSION_XML2=2.9.4
-VERSION_GSF=1.14.41
+VERSION_GLIB=2.54.2
+VERSION_XML2=2.9.7
+VERSION_GSF=1.14.42
 VERSION_EXIF=0.6.21
-VERSION_LCMS2=2.8
+VERSION_LCMS2=2.9
 VERSION_JPEG=1.5.2
-VERSION_PNG16=1.6.32
+VERSION_PNG16=1.6.34
 VERSION_WEBP=0.6.0
 VERSION_TIFF=4.0.8
 VERSION_ORC=0.4.27
-VERSION_GDKPIXBUF=2.36.8
-VERSION_FREETYPE=2.8
-VERSION_EXPAT=2.2.3
-VERSION_FONTCONFIG=2.12.4
-VERSION_HARFBUZZ=1.4.8
+VERSION_GDKPIXBUF=2.36.11
+VERSION_FREETYPE=2.8.1
+VERSION_EXPAT=2.2.5
+VERSION_FONTCONFIG=2.12.6
+VERSION_HARFBUZZ=1.7.1
 VERSION_PIXMAN=0.34.0
 VERSION_CAIRO=1.14.10
-VERSION_PANGO=1.40.10
+VERSION_PANGO=1.40.13
 VERSION_CROCO=0.6.12
 VERSION_SVG=2.40.18
 VERSION_GIF=5.1.4
 
 # Least out-of-sync Sourceforge mirror
-SOURCEFORGE_MIRROR=netix
+SOURCEFORGE_BASE_URL=https://netix.dl.sourceforge.net/project/
+
+# Remove patch version component
+without_patch() {
+  echo "$1" | sed "s/\.[0-9]*$//"
+}
+
+# Check for newer versions
+ALL_AT_VERSION_LATEST=true
+version_latest() {
+  VERSION_LATEST=$(curl -s https://release-monitoring.org/api/project/$3 | jq -r '.version')
+  if [ "$VERSION_LATEST" != "$2" ]; then
+    ALL_AT_VERSION_LATEST=false
+    echo "$1 version $2 has been superseded by $VERSION_LATEST"
+  fi
+}
+version_latest "zlib" "$VERSION_ZLIB" "5303"
+version_latest "ffi" "$VERSION_FFI" "1611"
+version_latest "glib" "$VERSION_GLIB" "10024"
+version_latest "xml2" "$VERSION_XML2" "1783"
+version_latest "gsf" "$VERSION_GSF" "1980"
+version_latest "exif" "$VERSION_EXIF" "1607"
+version_latest "lcms2" "$VERSION_LCMS2" "9815"
+version_latest "jpeg" "$VERSION_JPEG" "1648"
+version_latest "png" "$VERSION_PNG16" "15294"
+version_latest "webp" "$VERSION_WEBP" "1761"
+version_latest "tiff" "$VERSION_TIFF" "13521"
+version_latest "orc" "$VERSION_ORC" "2573"
+version_latest "gdkpixbuf" "$VERSION_GDKPIXBUF" "9533"
+version_latest "freetype" "$VERSION_FREETYPE" "854"
+version_latest "expat" "$VERSION_EXPAT" "770"
+version_latest "fontconfig" "$VERSION_FONTCONFIG" "827"
+version_latest "harfbuzz" "$VERSION_HARFBUZZ" "1299"
+version_latest "pixman" "$VERSION_PIXMAN" "3648"
+#version_latest "cairo" "$VERSION_CAIRO" "247" # latest version in release monitoring does not exist
+version_latest "pango" "$VERSION_PANGO" "11783"
+version_latest "croco" "$VERSION_CROCO" "11787"
+#version_latest "svg" "$VERSION_SVG" "5420" # latest version requires rust toolchain
+version_latest "gif" "$VERSION_GIF" "1158"
+if [ "$ALL_AT_VERSION_LATEST" = "false" ]; then exit 1; fi
+
+# Download and build dependencies from source
 
 mkdir ${DEPS}/zlib
 curl -Ls http://zlib.net/zlib-${VERSION_ZLIB}.tar.xz | tar xJC ${DEPS}/zlib --strip-components=1
@@ -57,7 +98,7 @@ cd ${DEPS}/ffi
 make install-strip
 
 mkdir ${DEPS}/glib
-curl -Ls https://download.gnome.org/sources/glib/2.53/glib-${VERSION_GLIB}.tar.xz | tar xJC ${DEPS}/glib --strip-components=1
+curl -Ls https://download.gnome.org/sources/glib/$(without_patch $VERSION_GLIB)/glib-${VERSION_GLIB}.tar.xz | tar xJC ${DEPS}/glib --strip-components=1
 cd ${DEPS}/glib
 echo glib_cv_stack_grows=no >>glib.cache
 echo glib_cv_uscore=no >>glib.cache
@@ -74,24 +115,21 @@ cd ${DEPS}/xml2
 make install-strip
 
 mkdir ${DEPS}/gsf
-curl -Ls https://download.gnome.org/sources/libgsf/1.14/libgsf-${VERSION_GSF}.tar.xz | tar xJC ${DEPS}/gsf --strip-components=1
+curl -Ls https://download.gnome.org/sources/libgsf/$(without_patch $VERSION_GSF)/libgsf-${VERSION_GSF}.tar.xz | tar xJC ${DEPS}/gsf --strip-components=1
 cd ${DEPS}/gsf
 ./configure --host=${CHOST} --prefix=${TARGET} --enable-shared --disable-static --disable-dependency-tracking
 make install-strip
 
 mkdir ${DEPS}/exif
-curl -Ls http://${SOURCEFORGE_MIRROR}.dl.sourceforge.net/project/libexif/libexif/${VERSION_EXIF}/libexif-${VERSION_EXIF}.tar.bz2 | tar xjC ${DEPS}/exif --strip-components=1
+curl -Ls ${SOURCEFORGE_BASE_URL}libexif/libexif/${VERSION_EXIF}/libexif-${VERSION_EXIF}.tar.bz2 | tar xjC ${DEPS}/exif --strip-components=1
 cd ${DEPS}/exif
 autoreconf -fiv
 ./configure --host=${CHOST} --prefix=${TARGET} --enable-shared --disable-static --disable-dependency-tracking
 make install-strip
 
 mkdir ${DEPS}/lcms2
-curl -Ls http://${SOURCEFORGE_MIRROR}.dl.sourceforge.net/project/lcms/lcms/${VERSION_LCMS2}/lcms2-${VERSION_LCMS2}.tar.gz | tar xzC ${DEPS}/lcms2 --strip-components=1
+curl -Ls ${SOURCEFORGE_BASE_URL}lcms/lcms/${VERSION_LCMS2}/lcms2-${VERSION_LCMS2}.tar.gz | tar xzC ${DEPS}/lcms2 --strip-components=1
 cd ${DEPS}/lcms2
-# Apply patches for lcms2 vulnerabilities reported since v2.8
-VERSION_LCMS2_GIT_MASTER_SHA=$(curl -Ls https://api.github.com/repos/mm2/Little-CMS/git/refs/heads/master | jq -r '.object.sha' | head -c7)
-curl -Ls https://github.com/mm2/Little-CMS/compare/lcms2.8...master.patch | patch -p1 -t || true
 ./configure --host=${CHOST} --prefix=${TARGET} --enable-shared --disable-static --disable-dependency-tracking
 make install-strip
 
@@ -103,7 +141,7 @@ autoreconf -fiv
 make install-strip
 
 mkdir ${DEPS}/png16
-curl -Ls https://ftp-osl.osuosl.org/pub/libpng/src/libpng16/libpng-${VERSION_PNG16}.tar.xz | tar xJC ${DEPS}/png16 --strip-components=1
+curl -Ls ${SOURCEFORGE_BASE_URL}libpng/libpng16/${VERSION_PNG16}/libpng-${VERSION_PNG16}.tar.xz | tar xJC ${DEPS}/png16 --strip-components=1
 cd ${DEPS}/png16
 ./configure --host=${CHOST} --prefix=${TARGET} --enable-shared --disable-static --disable-dependency-tracking
 make install-strip
@@ -132,7 +170,7 @@ cd ${TARGET}/lib
 rm -rf liborc-test-*
 
 mkdir ${DEPS}/gdkpixbuf
-curl -Ls https://download.gnome.org/sources/gdk-pixbuf/2.36/gdk-pixbuf-${VERSION_GDKPIXBUF}.tar.xz | tar xJC ${DEPS}/gdkpixbuf --strip-components=1
+curl -Ls https://download.gnome.org/sources/gdk-pixbuf/$(without_patch $VERSION_GDKPIXBUF)/gdk-pixbuf-${VERSION_GDKPIXBUF}.tar.xz | tar xJC ${DEPS}/gdkpixbuf --strip-components=1
 cd ${DEPS}/gdkpixbuf
 touch gdk-pixbuf/loaders.cache
 LD_LIBRARY_PATH=${TARGET}/lib \
@@ -142,13 +180,13 @@ LD_LIBRARY_PATH=${TARGET}/lib \
 make install-strip
 
 mkdir ${DEPS}/freetype
-curl -Ls http://${SOURCEFORGE_MIRROR}.dl.sourceforge.net/project/freetype/freetype2/${VERSION_FREETYPE}/freetype-${VERSION_FREETYPE}.tar.gz | tar xzC ${DEPS}/freetype --strip-components=1
+curl -Ls ${SOURCEFORGE_BASE_URL}freetype/freetype2/${VERSION_FREETYPE}/freetype-${VERSION_FREETYPE}.tar.gz | tar xzC ${DEPS}/freetype --strip-components=1
 cd ${DEPS}/freetype
 ./configure --host=${CHOST} --prefix=${TARGET} --enable-shared --disable-static
 make install
 
 mkdir ${DEPS}/expat
-curl -Ls http://${SOURCEFORGE_MIRROR}.dl.sourceforge.net/project/expat/expat/${VERSION_EXPAT}/expat-${VERSION_EXPAT}.tar.bz2 | tar xjC ${DEPS}/expat --strip-components=1
+curl -Ls ${SOURCEFORGE_BASE_URL}expat/expat/${VERSION_EXPAT}/expat-${VERSION_EXPAT}.tar.bz2 | tar xjC ${DEPS}/expat --strip-components=1
 cd ${DEPS}/expat
 ./configure --host=${CHOST} --prefix=${TARGET} --enable-shared --disable-static
 make install
@@ -181,32 +219,32 @@ cd ${DEPS}/cairo
 make install-strip
 
 mkdir ${DEPS}/pango
-curl -Ls https://download.gnome.org/sources/pango/1.40/pango-${VERSION_PANGO}.tar.xz | tar xJC ${DEPS}/pango --strip-components=1
+curl -Ls https://download.gnome.org/sources/pango/$(without_patch $VERSION_PANGO)/pango-${VERSION_PANGO}.tar.xz | tar xJC ${DEPS}/pango --strip-components=1
 cd ${DEPS}/pango
 ./autogen.sh
 ./configure --host=${CHOST} --prefix=${TARGET} --enable-shared --disable-static --disable-dependency-tracking
 make install-strip
 
 mkdir ${DEPS}/croco
-curl -Ls https://download.gnome.org/sources/libcroco/0.6/libcroco-${VERSION_CROCO}.tar.xz | tar xJC ${DEPS}/croco --strip-components=1
+curl -Ls https://download.gnome.org/sources/libcroco/$(without_patch $VERSION_CROCO)/libcroco-${VERSION_CROCO}.tar.xz | tar xJC ${DEPS}/croco --strip-components=1
 cd ${DEPS}/croco
 ./configure --host=${CHOST} --prefix=${TARGET} --enable-shared --disable-static --disable-dependency-tracking
 make install-strip
 
 mkdir ${DEPS}/svg
-curl -Ls https://download.gnome.org/sources/librsvg/2.40/librsvg-${VERSION_SVG}.tar.xz | tar xJC ${DEPS}/svg --strip-components=1
+curl -Ls https://download.gnome.org/sources/librsvg/$(without_patch $VERSION_SVG)/librsvg-${VERSION_SVG}.tar.xz | tar xJC ${DEPS}/svg --strip-components=1
 cd ${DEPS}/svg
 ./configure --host=${CHOST} --prefix=${TARGET} --enable-shared --disable-static --disable-dependency-tracking --disable-introspection --disable-tools --disable-pixbuf-loader
 make install-strip
 
 mkdir ${DEPS}/gif
-curl -Ls http://${SOURCEFORGE_MIRROR}.dl.sourceforge.net/project/giflib/giflib-${VERSION_GIF}.tar.gz | tar xzC ${DEPS}/gif --strip-components=1
+curl -Ls ${SOURCEFORGE_BASE_URL}giflib/giflib-${VERSION_GIF}.tar.gz | tar xzC ${DEPS}/gif --strip-components=1
 cd ${DEPS}/gif
 ./configure --host=${CHOST} --prefix=${TARGET} --enable-shared --disable-static --disable-dependency-tracking
 make install-strip
 
 mkdir ${DEPS}/vips
-curl -Ls https://github.com/jcupitt/libvips/releases/download/v${VERSION_VIPS}-alpha4/vips-${VERSION_VIPS}-3.tar.gz | tar xzC ${DEPS}/vips --strip-components=1
+curl -Ls https://github.com/jcupitt/libvips/releases/download/v${VERSION_VIPS}-alpha5/vips-${VERSION_VIPS}-4.tar.gz | tar xzC ${DEPS}/vips --strip-components=1
 cd ${DEPS}/vips
 ./configure --host=${CHOST} --prefix=${TARGET} --enable-shared --disable-static --disable-dependency-tracking \
   --disable-debug --disable-introspection --without-python --without-fftw \
