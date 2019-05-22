9171341adccfc97e40b87e25a2c9b44087766de2
Lovell Fuller <github@lovell.info>
2019-05-22 20:53:27 +0100

    Upgrade version of gcc in ARMv6 container

--- linux-armv6/Toolchain.cmake
+++ linux-armv6/Toolchain.cmake
@@ -2,9 +2,9 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_SYSTEM_PROCESSOR armv6-a)
 
-SET(CMAKE_C_COMPILER /root/tools/x64-gcc-6.3.1/arm-rpi-linux-gnueabihf/bin/arm-rpi-linux-gnueabihf-gcc)
-SET(CMAKE_AR /root/tools/x64-gcc-6.3.1/arm-rpi-linux-gnueabihf/bin/arm-rpi-linux-gnueabihf-gcc-ar)
-SET(CMAKE_RANLIB /root/tools/x64-gcc-6.3.1/arm-rpi-linux-gnueabihf/bin/arm-rpi-linux-gnueabihf-gcc-ranlib)
+SET(CMAKE_C_COMPILER /root/tools/x64-gcc-6.5.0/arm-rpi-linux-gnueabihf/bin/arm-rpi-linux-gnueabihf-gcc)
+SET(CMAKE_AR /root/tools/x64-gcc-6.5.0/arm-rpi-linux-gnueabihf/bin/arm-rpi-linux-gnueabihf-gcc-ar)
+SET(CMAKE_RANLIB /root/tools/x64-gcc-6.5.0/arm-rpi-linux-gnueabihf/bin/arm-rpi-linux-gnueabihf-gcc-ranlib)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
