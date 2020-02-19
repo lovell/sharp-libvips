4a133e461be92e7224df55478502f18ba8cd0251
Lovell Fuller <github@lovell.info>
2020-02-19 10:30:52 +0000

    Switch to MXE-based libvips Windows builds #24

--- build/win.sh
+++ build/win.sh
@@ -7,7 +7,7 @@ VERSION_VIPS_MINOR=$(echo $VERSION_VIPS | cut -d. -f2)
 # Fetch and unzip
 mkdir /vips
 cd /vips
-curl -LOs https://github.com/lovell/build-win64/releases/download/v${VERSION_VIPS}/vips-dev-w64-web-${VERSION_VIPS}.zip
+curl -LOs https://github.com/libvips/build-win64-mxe/releases/download/v${VERSION_VIPS}/vips-dev-w64-web-${VERSION_VIPS}.zip
 unzip vips-dev-w64-web-${VERSION_VIPS}.zip
 
 # Clean and zip
@@ -17,41 +17,6 @@ cp bin/*.dll lib/
 
 # Create platform.json
 printf "\"${PLATFORM}\"" >platform.json
-
-# Create versions.json
-curl -Os https://raw.githubusercontent.com/lovell/build-win64/v${VERSION_VIPS}/${VERSION_VIPS_MAJOR}.${VERSION_VIPS_MINOR}/vips.modules
-version_of() {
-  xmllint --xpath "string(/moduleset/autotools[@id='$1']/branch/@version | /moduleset/cmake[@id='$1']/branch/@version | /moduleset/meson[@id='$1']/branch/@version)" vips.modules
-}
-printf "{\n\
-  \"cairo\": \"$(version_of cairo)\",\n\
-  \"croco\": \"$(version_of libcroco)\",\n\
-  \"exif\": \"$(version_of libexif)\",\n\
-  \"expat\": \"$(version_of expat)\",\n\
-  \"ffi\": \"$(version_of libffi)\",\n\
-  \"fontconfig\": \"$(version_of fontconfig)\",\n\
-  \"freetype\": \"$(version_of freetype)\",\n\
-  \"fribidi\": \"$(version_of fribidi)\",\n\
-  \"gdkpixbuf\": \"$(version_of gdk-pixbuf)\",\n\
-  \"gettext\": \"$(version_of gettext)\",\n\
-  \"gif\": \"$(version_of giflib)\",\n\
-  \"glib\": \"$(version_of glib)\",\n\
-  \"gsf\": \"$(version_of libgsf)\",\n\
-  \"harfbuzz\": \"$(version_of harfbuzz)\",\n\
-  \"jpeg\": \"$(version_of libjpeg-turbo)\",\n\
-  \"lcms\": \"$(version_of lcms)\",\n\
-  \"pango\": \"$(version_of pango)\",\n\
-  \"pixman\": \"$(version_of pixman)\",\n\
-  \"png\": \"$(version_of libpng)\",\n\
-  \"svg\": \"$(version_of librsvg)\",\n\
-  \"tiff\": \"$(version_of tiff)\",\n\
-  \"vips\": \"${VERSION_VIPS}\",\n\
-  \"webp\": \"$(version_of webp)\",\n\
-  \"xml\": \"$(version_of libxml2)\",\n\
-  \"zlib\": \"$(version_of zlib)\"\n\
-}" >versions.json
-rm vips.modules
-cat versions.json
 
 # Add third-party notices
 curl -Os https://raw.githubusercontent.com/lovell/sharp-libvips/master/THIRD-PARTY-NOTICES.md
