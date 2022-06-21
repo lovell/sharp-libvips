8beec3c909557d20fcc5712c0e6b3830dd64dd26
Lovell Fuller <github@lovell.info>
2022-06-21 21:10:00 +0100

    Linux: upgrade Alpine to 3.12 (3.11 reached EOL last year)
    Alpine 3.12 provides same musl version

--- build.sh
+++ build.sh
@@ -74,7 +74,7 @@ if ! [ -x "$(command -v docker)" ]; then
 fi
 
 # Update base images
-for baseimage in centos:7 debian:buster debian:bullseye alpine:3.11; do
+for baseimage in centos:7 debian:buster debian:bullseye alpine:3.12; do
   docker pull $baseimage
 done
 
