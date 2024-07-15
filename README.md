5705acb6507cba66f8abc5497070458568c84f97
Sumit Dubey <sumit.dubey2@ibm.com>
2024-07-15 15:40:30 +0000

    ppc64le support

--- README.md
+++ README.md
@@ -23,6 +23,7 @@ the same shared libraries within multiple containers.
 * [ARMv7-A glibc](platforms/linux-armv7/Dockerfile)
 * [ARM64v8-A glibc](platforms/linux-arm64v8/Dockerfile)
 * [ARM64v8-A musl](platforms/linuxmusl-arm64v8/Dockerfile)
+* [ppc64le glibc](platforms/linux-ppc64le/Dockerfile)
 * [s390x glibc](platforms/linux-s390x/Dockerfile)
 
 ### Windows
--- build.sh
+++ build.sh
@@ -20,6 +20,7 @@ if [ $# -lt 1 ]; then
   echo "- linux-armv7"
   echo "- linux-arm64v8"
   echo "- linuxmusl-arm64v8"
+  echo "- linux-ppc64le"
   echo "- linux-s390x"
   echo "- darwin-x64"
   echo "- darwin-arm64v8"
@@ -101,7 +102,7 @@ for flavour in win32-ia32 win32-x64 win32-arm64v8; do
 done
 
 # Linux (x64, ARMv6, ARMv7, ARM64v8)
-for flavour in linux-x64 linuxmusl-x64 linux-armv6 linux-armv7 linux-arm64v8 linuxmusl-arm64v8 linux-s390x; do
+for flavour in linux-x64 linuxmusl-x64 linux-armv6 linux-armv7 linux-arm64v8 linuxmusl-arm64v8 linux-ppc64le linux-s390x; do
   if [ $PLATFORM = "all" ] || [ $PLATFORM = $flavour ]; then
     echo "Building $flavour..."
     docker build -t vips-dev-$flavour platforms/$flavour
