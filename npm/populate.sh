b5cd3b7079dce10232cef61374e882e32ca836bd
Dennis Ameling <dennis@dennisameling.com>
2025-04-22 18:45:47 +0200

    Publish sharp-libvips-win32-arm64 NPM package

--- npm/populate.sh
+++ npm/populate.sh
@@ -58,6 +58,7 @@ for platform in $PLATFORMS; do
 done
 download_extract "win32-ia32"
 download_extract "win32-x64"
+download_extract "win32-arm64"
 
 # Common header and source files
 cp -r npm/linux-x64/{include,versions.json,THIRD-PARTY-NOTICES.md} npm/dev/
